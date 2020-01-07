1f61afa10c18650bff76293e6de51abc25ac7d23
ebertmota <ebertmota03@gmail.com>
2020-01-06 22:20:59 -0300

    building first views files, and header css design

--- frontend/public/bundle.js
+++ frontend/public/bundle.js
@@ -93,7 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-alert('index rodando');
+console.log('index rodando');
 
 /***/ })
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,1 @@
-alert('index rodando')
+console.log('index rodando')
