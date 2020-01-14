faf7cbdeccff0131b2f5246e991f23056106021b
ebertmota <ebert.mota03@gmail.com>
2020-01-13 23:24:12 -0300

    studens and login/register routes and styles

--- routes.js
+++ routes.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const route = express.Router();
 const HomeController = require('./src/controllers/HomeController');
+const StudentsController = require('./src/controllers/StudentsController');
+const AuthenticantionController = require('./src/controllers/AuthenticationController')
 
 route.get('/', HomeController.index);
+
+route.get('/students/status', StudentsController.status); 
+route.get('/students/new',StudentsController.new);
+
+route.get('/auth/login', AuthenticantionController.login);
+route.get('/auth/register', AuthenticantionController.register);
 
 module.exports = route;
---
+++ src/controllers/StudentsController.js
@@ -0,0 +1,7 @@
+module.exports.new = (req,res) => {
+  res.render('students');
+};
+
+module.exports.status = (req, res) => {
+  res.render('status');
+};
